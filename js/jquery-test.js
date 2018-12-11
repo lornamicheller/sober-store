16bd5886ce866b993b909a5410936d97bec0582f
lornamicheller <37231071+lornamicheller@users.noreply.github.com>
2018-12-11 14:37:38 -0400

    first-commit

---
+++ js/jquery-test.js
@@ -0,0 +1,41 @@
+/* global $ */
+
+
+// // variables
+
+// let firstName = 'Rebeca';
+// let lastName = 'Alicea';
+
+// console.log(firstName, lastName);
+
+// // object
+// let person = {
+//   name: 'Lorna',
+//   age: 27
+// };
+
+// person.name = 'Rose';
+
+// // arrays
+// let selectedColors = ['red', 'blue'];
+// selectedColors[2] = 'pink';
+
+// console.log(selectedColors);
+
+// // performinfg a task
+// function greet(name, lastName) {
+//   console.log('Hello ' + name + ' ' + lastName);
+// }
+
+// greet('John', 'Smith');
+
+// // calculates a value
+// function square(number) {
+//  return number * number;
+// }
+
+// console.log(square(2));
+
+$('.services').on('click', () => {
+  ('.active').removeClass('active');
+})
