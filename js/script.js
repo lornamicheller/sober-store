a57cfde2bd34037ea89b62091f41096f3a791265
lornamicheller <37231071+lornamicheller@users.noreply.github.com>
2018-12-12 16:34:34 -0400

    sign-in-form

--- js/script.js
+++ js/script.js
@@ -7,19 +7,27 @@ $(document).on('click', 'ul li', function () {
 })
 
 $('#shop-button').on('mouseenter', () => {
-  $('.shop-box').slideToggle(100);
+  $('.shop-box').slideToggle(300);
 })
 
 $('#shop-button').on('mouseleave', () => {
-  $('.shop-box').slideToggle(100);
+  $('.shop-box').slideToggle(300);
 })
 
 $('#sign-in-button').on('mouseenter', () => {
-  $('.sign-in-form').slideToggle(100);
+  $('.sign-in-form').slideToggle(300);
 })
 
 $('#sign-in-button').on('mouseleave', () => {
-  $('.sign-in-form').slideToggle(100);
+  $('.sign-in-form').slideToggle(300);
+})
+
+$('#cart-button').on('mouseenter', () => {
+  $('.cart-container').slideToggle(300);
+})
+
+$('#cart-button').on('mouseleave', () => {
+  $('.cart-container').slideToggle(300);
 })
 
 });
