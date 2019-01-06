ca66baee9a19805ebd7a60f7dcd72b75c9cf794c
lornamicheller <37231071+lornamicheller@users.noreply.github.com>
2019-01-06 17:16:34 -0400

    commit-images

--- js/script.js
+++ js/script.js
@@ -21,21 +21,21 @@ $('#cart-button').on('click', () => {
   $('.cart-container').slideToggle(300);
 })
 
-$('.arrow-button-1').on('click', () => {
-  $('.price-one').slideToggle(300);
-})
+// $('.arrow-button-1').on('click', () => {
+//   $('.price-one').slideToggle(300);
+// })
 
-$('.arrow-button-2').on('click', () => {
-  $('.price-two').slideToggle(300);
-})
+// $('.arrow-button-2').on('click', () => {
+//   $('.price-two').slideToggle(300);
+// })
 
-$('.arrow-button-3').on('click', () => {
-  $('.price-three').slideToggle(300);
-})
+// $('.arrow-button-3').on('click', () => {
+//   $('.price-three').slideToggle(300);
+// })
 
-$('.arrow-button-4').on('click', () => {
-  $('.price-four').slideToggle(300);
-})
+// $('.arrow-button-4').on('click', () => {
+//   $('.price-four').slideToggle(300);
+// })
 
 $('.fa-angle-double-down').on('click', () => {
   event.preventDefault();
